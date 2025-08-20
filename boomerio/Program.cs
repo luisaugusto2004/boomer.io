01680f785958a3aae562bf59282d29ed728086b8
Luis Augusto <luisaugusto010203@gmail.com>
2025-08-20 15:36:14 -0300

    Releasing swagger in all environments

--- boomerio/Program.cs
+++ boomerio/Program.cs
@@ -40,11 +40,9 @@ namespace boomerio
             app.UseMiddleware<ExceptionHandlingMiddleware>();
             app.UseCors("AllowAll");
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsProduction())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+       
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             if (!app.Environment.IsEnvironment("Docker"))
             {
